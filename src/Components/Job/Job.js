71b1eaab4ca4b54ad1cc1327ef116017fb33ecdb
Nicolai-Bak <nicolai.bak@gmail.com>
2022-05-16 19:02:48 +0200

    small changes

--- src/Components/Job/Job.js
+++ src/Components/Job/Job.js
@@ -1,5 +1,5 @@
 import "./Job.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { MdLocationOn } from "react-icons/md";
 import { IoTrashSharp, IoClose, IoClipboard } from "react-icons/io5";
 
@@ -12,8 +12,9 @@ const Job = (props) => {
   const [selectedText, setSelectedText] = useState();
   const [selectedAmount, setSelectedAmount] = useState();
   const [options, setOptions] = useState([]);
+  const [modelAdded, setModelAdded] = useState(false);
 
-  async function getModels() {
+  const getModels = useCallback(async () => {
     let url = "https://localhost:7181/api/Models";
     try {
       let response = await fetch(url, {
@@ -27,7 +28,7 @@ const Job = (props) => {
       if (response.ok) {
         let options = await response.json();
         setOptions(options);
-        setSelectedModel(options[0].efModelId)
+        setSelectedModel(options[0].efModelId);
       } else {
         alert("Server returned: " + response.statusText);
       }
@@ -35,11 +36,9 @@ const Job = (props) => {
       alert("Error: " + err);
     }
     return options;
-  }
+  }, []);
 
-  
-  
-  async function getModelsAddedToJob() {
+  const getModelsAddedToJob = useCallback(async () => {
     let url = `https://localhost:7181/api/Jobs/${props.jobId}`;
     try {
       let response = await fetch(url, {
@@ -60,71 +59,103 @@ const Job = (props) => {
       alert("Error: " + err);
     }
     return;
-  }
+  }, [props.jobId]);
 
   useEffect(() => {
-    getModelsAddedToJob()
-  }, [getModelsAddedToJob])
+    getModelsAddedToJob();
+  }, [getModelsAddedToJob]);
 
+  useEffect(() => {
+    getModels();
+  }, [getModels]);
+
+  useEffect(() => {
+    getModelsAddedToJob();
+    getModels();
+    setModelAdded(false);
+  }, [modelAdded]);
 
   const displayModelsAddedToJob = addedModels.map((model) => {
     return (
-        <li className="model-name">{model.firstName} <IoTrashSharp className="delete-model-button" key={model.email} onClick={() => removeModelHandler(model.email)}/></li>
-    )});
+      <li className="model-name">
+        {model.firstName}{" "}
+        <IoTrashSharp
+          className="delete-model-button"
+          key={model.email}
+          onClick={() => removeModelHandler(model.email)}
+        />
+      </li>
+    );
+  });
 
-    async function removeModelHandler  (modelemail)  {
+  async function removeModelHandler(modelemail) {
+    console.log(modelemail);
+    let allModels;
+    let model;
 
-      console.log(modelemail);
-      let model;
-      
-        let allModels = await getModels()
-        console.log(allModels)
-        model = allModels.find(option => option.email === modelemail);
-      
-      console.log(model)
-      var url = `https://localhost:7181/api/Jobs/${props.jobId}/model/${model.efModelId}`;
-      await fetch(url, {
-        method: "DELETE",
-        credentials: "include",
-        headers: {
+    let urlGet = `https://localhost:7181/api/Models`;
+    try {
+      let response = await fetch(urlGet, {
+        method: "GET",
+        headers: new Headers({
           Authorization: "Bearer " + localStorage.getItem("token"),
           "Content-Type": "application/json",
-        },
-      })
-        .then((responseJson) => {
-        })
-        .catch((error) => alert("Something bad happened: " + error));
-    };
-  
+        }),
+      });
+
+      if (response.ok) {
+        allModels = await response.json();
+        console.log(allModels);
+        model = allModels.find((option) => option.email === modelemail);
+        console.log(model);
+        setModelAdded(true)
+      } else {
+        alert("Server returned: " + response.statusText);
+      }
+    } catch (err) {
+      alert("Error: " + err);
+    }
+
+    var url = `https://localhost:7181/api/Jobs/${props.jobId}/model/${model.efModelId}`;
+    await fetch(url, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+    })
+      .then((responseJson) => {})
+      .catch((error) => alert("Something bad happened: " + error));
+  }
 
   const showModels = () => {
     setShowModelsToAdd(!showModelsToAdd);
 
-    getModels();
-    
+    // getModels();
   };
 
   const showExpenses = () => {
     setShowExpensesToAdd(!showExpensesToAdd);
   };
 
-
   const selectedModelHandler = (event) => {
     setSelectedModel(event.target.value);
-  }
+  };
   const selectedDateHandler = (event) => {
     setSelectedDate(event.target.value);
-  }
+  };
   const selectedTextHandler = (event) => {
     setSelectedText(event.target.value);
-  }
+  };
   const selectedAmountHandler = (event) => {
     setSelectedAmount(event.target.value);
-  }
+  };
 
   const submitModelHandler = (event) => {
     event.preventDefault();
     var url = `https://localhost:7181/api/Jobs/${props.jobId}/model/${selectedModel}`;
+
     fetch(url, {
       method: "POST", // Or PUT
       // body: JSON.stringify(this.form), // assumes your data is in a
@@ -135,14 +166,16 @@ const Job = (props) => {
         "Content-Type": "application/json",
       },
     })
-      .then((responseJson) => {
+      .then((response) => {
+        if (response.ok) {
+          setModelAdded(true);
+        }
         // this.response = responseJson;
       })
       .catch((error) => alert("Something bad happened: " + error));
 
-      getModelsAddedToJob();
+    // getModelsAddedToJob();
   };
-
 
   const submitExpenseHandler = (event) => {
     event.preventDefault();
@@ -154,9 +187,9 @@ const Job = (props) => {
       date: selectedDate,
       text: selectedText,
       amount: selectedAmount,
-  }
+    };
 
-    console.log(expenseData)
+    console.log(expenseData);
 
     fetch(url, {
       method: "POST", // Or PUT
@@ -172,10 +205,9 @@ const Job = (props) => {
       })
       .catch((error) => alert("Something bad happened: " + error));
 
-    setSelectedDate("")
-    setSelectedText("")
-    setSelectedAmount("")
-    
+    setSelectedDate("");
+    setSelectedText("");
+    setSelectedAmount("");
   };
 
   let toggleAddModelOption = null;
@@ -210,7 +242,6 @@ const Job = (props) => {
     );
   }
 
-
   // useEffect(() => {
   //   getModels()
   // }, [getModels])
@@ -219,10 +250,12 @@ const Job = (props) => {
     <div className="job">
       <p className="job-title">
         {props.customer}
-        {props.modelId == -1 && <IoTrashSharp
-          className="trash-icon"
-          onClick={() => props.onRemoveJob(props.jobId)}
-        />}
+        {props.modelId == -1 && (
+          <IoTrashSharp
+            className="trash-icon"
+            onClick={() => props.onRemoveJob(props.jobId)}
+          />
+        )}
       </p>
       <hr />
       <p>
@@ -231,28 +264,47 @@ const Job = (props) => {
         {props.location}
       </p>
       <hr />
-      {props.modelId == -1 && ( <div>
-      <ul className="list-of-model-names">{displayModelsAddedToJob}</ul>
-      <div className="expand-button" onClick={showModels}>
-        {toggleAddModelOption}
-      </div>
-      </div>)}
-      {props.modelId != -1 && ( <div>
-      <div className="expand-button" onClick={showExpenses}>
-        {toggleAddModelExpense}
-      </div>
-      </div>)}
+      {props.modelId == -1 && (
+        <div>
+          <ul className="list-of-model-names">{displayModelsAddedToJob}</ul>
+          <div className="expand-button" onClick={showModels}>
+            {toggleAddModelOption}
+          </div>
+        </div>
+      )}
+      {props.modelId != -1 && (
+        <div>
+          <div className="expand-button" onClick={showExpenses}>
+            {toggleAddModelExpense}
+          </div>
+        </div>
+      )}
 
       {showExpensesToAdd && (
-        <form onSubmit={submitExpenseHandler} className="add-model-form-wrapper">
+        <form
+          onSubmit={submitExpenseHandler}
+          className="add-model-form-wrapper"
+        >
           <div className="add-expense-form">
-          <label className="expense-labels">Date</label>
-          <input type="date" value={selectedDate} onChange={selectedDateHandler}/>
-          <label className="expense-labels">text</label>
-          <input type="text" value={selectedText} onChange={selectedTextHandler}/>
-          <label className="expense-labels">amount</label>
+            <label className="expense-labels">Date</label>
+            <input
+              type="date"
+              value={selectedDate}
+              onChange={selectedDateHandler}
+            />
+            <label className="expense-labels">text</label>
+            <input
+              type="text"
+              value={selectedText}
+              onChange={selectedTextHandler}
+            />
+            <label className="expense-labels">amount</label>
           </div>
-          <input type="number" value={selectedAmount} onChange={selectedAmountHandler}/>
+          <input
+            type="number"
+            value={selectedAmount}
+            onChange={selectedAmountHandler}
+          />
           <button type="submit" className="button add-model-button">
             Add Expense
           </button>
@@ -263,7 +315,9 @@ const Job = (props) => {
         <form onSubmit={submitModelHandler} className="add-model-form-wrapper">
           <select value={selectedModel} onChange={selectedModelHandler}>
             {options.map((option) => (
-              <option value={option.efModelId} key={option.efModelId}>{option.firstName}</option>
+              <option value={option.efModelId} key={option.efModelId}>
+                {option.firstName}
+              </option>
             ))}
           </select>{" "}
           <button type="submit" className="button add-model-button">
